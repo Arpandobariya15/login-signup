334b020aca459f2af5fc3437662430d069d65a01
Arpandobariya15 <work.arpan8980@gmail.com>
2024-12-26 09:49:27 +0530

    first commit

---
+++ frondend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='d-flex justify-content-center align-items-center p-3 text-white bg-dark'style={{borderTop:"1px solid white"}}>
+      <p>Devloped by Arpan Dobariya</p>
+    </div>
+  );
+}
+
+export default Footer;
